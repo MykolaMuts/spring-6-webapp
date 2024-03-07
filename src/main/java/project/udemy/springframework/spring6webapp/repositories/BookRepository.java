4d8ef7ca8365e9f4d7ccb96af8f342d5ad452e04
MykolaMuts <muts.mykola@gmail.com>
2024-03-07 04:16:43 +0100

    Spring Data Repositories

---
+++ src/main/java/project/udemy/springframework/spring6webapp/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package project.udemy.springframework.spring6webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import project.udemy.springframework.spring6webapp.domain.Book;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
